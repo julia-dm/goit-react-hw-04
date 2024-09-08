c9e47644c1ecfd4df613eeb4d35eec8a02c904bf
julia-dm <yulia.2508dmitruk@gmail.com>
2024-09-08 23:12:49 +0200

    done

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -5,13 +5,14 @@ export default function ImageCard({ image, onImageClick }) {
   const { urls, alt_description } = image;
 
   return (
-    <div className={css.imageContainer} onClick={() => onImageClick(image)}>
+    <div className={css.imageContainer} >
       <img
         src={urls.small}
         alt={alt_description}
         width="360"
         height="239"
         className={css.image}
+        onClick={() => onImageClick(image)}
       />
     </div>
   );
